e4c955630865ab5294a9f3a6ebbd40037098776b
SProga <shane.306@hotmail.com>
2021-05-16 02:30:40 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,121 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const path = require("path");
+const cors = require("cors");
+const ejsMate = require("ejs-mate");
+
+const sessionConfig = {
+  secret: "thisisnotagoodsecret",
+  resave: false,
+  saveUninitialized: false,
+};
+
+app.engine("ejs", ejsMate);
+app.use(session(sessionConfig));
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+//whenever you have this middle ware set up on every request object
+//incoming request will have a session property available
+//resave is a value that defaults to true , don't use default set manually
+//set resave to false
+//CONST
+const cart = new Array();
+PRODUCT_PRICES = { Banana: 7, Apple: 3, WaterMelon: 1, Kiwi: 3, Grapes: 5 };
+app.use((req, res, next) => {
+  res.locals.cart = req.session.cart;
+  next();
+});
+
+const allProducts = [
+  { name: "Banana", type: "fruit", price: 7, img: "bananas.svg" },
+  { name: "Apple", type: "fruit", price: 3, img: "apple.svg" },
+  { name: "WaterMelon", type: "fruit", price: 1, img: "watermelon.svg" },
+  { name: "Kiwi", type: "fruit", price: 3, img: "kiwi.svg" },
+  { name: "Grapes", type: "fruit", price: 5, img: "grapes.svg" },
+];
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/products", (req, res) => {
+  console.log(product);
+  res.send(req.session.cart);
+});
+
+app.get("/products/all", (req, res) => {
+  res.status(200).send(allProducts);
+});
+
+app.post("/cart", (req, res) => {
+  const product = req.body.cart.product;
+  const filter = allProducts.filter((item) => item["name"] === product);
+  const item = filter[0];
+  const isAdded = cart.includes(item);
+  if (!isAdded) {
+    item.qty = 1;
+    cart.push(item);
+  } else {
+    item.qty++;
+    item.price = item.price + PRODUCT_PRICES[item["name"]];
+  }
+  req.session.cart = cart;
+  res.send(item);
+});
+
+app.get("/cart/all", (req, res) => {
+  const products = req.session.cart;
+  res.json(products);
+});
+
+app.post("/cart/removeItem", (req, res) => {
+  const product = req.body.cart.product;
+  const filter = cart.filter((item) => item["name"] === product);
+  const item = filter[0];
+
+  if (cart && cart.length > 0) {
+    if (item.qty > 0) {
+      item.qty--;
+      item.price = item.price - PRODUCT_PRICES[item["name"]];
+    }
+    if (item.qty == 0) {
+      const index = cart.findIndex((item) => item["name"] === product);
+      cart.splice(index, 1);
+      item.price = PRODUCT_PRICES[item["name"]];
+    }
+    req.session.cart = cart;
+  } else {
+    req.session.cart = null;
+  }
+  res.send(item);
+});
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
+
+// app.get("/viewcount", (req, res) => {
+//     if (req.session.count) {
+//       req.session.count += 1;
+//     } else {
+//       req.session.count = 1;
+//     }
+//     res.send(`you have viewed this page ${req.session.count} times`);
+//   });
+
+//   app.get("/register", (req, res) => {
+//     const { username = "Anonymous" } = req.query;
+//     req.session.username = username;
+//     res.redirect("/greet");
+//   });
+
+//   app.get("/greet", (req, res) => {
+//     const { username } = req.session;
+//     res.send(`Welcome back, ${username}`);
+//   });
---
+++ public/js/session.js
@@ -0,0 +1,129 @@
+const getAllProducts = async () => {
+  return await axios("/products/all", { method: "get" })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+const itemListDiv = document.querySelector(".product-list");
+const products = getAllProducts();
+
+products.then((data) => {
+  data.forEach((product) => {
+    createProduct(product, itemListDiv);
+  });
+  addEventHandlers();
+});
+
+function addEventHandlers() {
+  let addItem = document.querySelectorAll(".add");
+  addItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      const product = item.parentElement.textContent;
+      addToCart(product);
+    });
+  });
+}
+
+const addToCart = async (product) => {
+  try {
+    return await axios
+      .post("/cart", {
+        cart: {
+          product: product,
+        },
+      })
+      .then((response) => {
+        let product = response.data;
+        checkoutProduct();
+      });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const createProduct = (product, container) => {
+  const img = document.createElement("IMG");
+  img.src = "/imgs/" + product["img"];
+  img.classList.add("icon");
+  const item = document.createElement("LI");
+  const productItem = product["name"];
+  const text = document.createTextNode(productItem);
+  const span = document.createElement("SPAN");
+  span.classList.add("add");
+  item.appendChild(text);
+  item.prepend(img);
+  item.appendChild(span);
+  container.appendChild(item);
+};
+
+const removeProduct = async (product) => {
+  return await axios
+    .post("/cart/removeItem", {
+      cart: {
+        product: product,
+      },
+    })
+    .then((response) => {
+      let product = response.data;
+      checkoutProduct();
+    })
+    .catch((err) => console.log(err));
+};
+
+const productQuantityHandlers = () => {
+  let removebutton = document.querySelectorAll(".remove");
+  removebutton.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const product = btn.parentElement.previousSibling.textContent;
+      removeProduct(product);
+    });
+  });
+};
+
+const checkoutProduct = async () => {
+  return await axios.get("/cart/all").then((response) => {
+    const product = response.data;
+    if (product.length) {
+      checkoutCart(product);
+    } else {
+      emptyCart();
+    }
+  });
+};
+
+const checkoutCart = function (product) {
+  let table = document.querySelector(".table");
+  table.innerHTML =
+    "<tr class='table-heading'><th>Item</th><th>Remove</th><th>Amount</th><th>Price</th></tr>";
+  if (product.length > 0) {
+    for (let i = 0; i < product.length; i++) {
+      let row = table.insertRow(i + 1);
+      let cell1 = row.insertCell(0);
+      let cell2 = row.insertCell(1);
+      let cell3 = row.insertCell(2);
+      let cell4 = row.insertCell(3);
+      const spanRemove = document.createElement("SPAN");
+      spanRemove.classList.add("remove");
+      cell1.innerHTML = "<td>" + product[i]["name"] + "</td>";
+      cell2.appendChild(spanRemove);
+      cell3.innerHTML = "<td>" + product[i]["qty"] + "</td>";
+      cell4.innerHTML = "<td>" + "$" + product[i]["price"] + "</td>";
+      productQuantityHandlers();
+    }
+  }
+};
+function emptyCart() {
+  let table = document.querySelector(".table");
+  table.innerHTML =
+    "<tr class='table-heading'><th>Item</th><th>Remove</th><th>Amount</th><th>Price</th></tr>";
+  let noticeRow = document.createElement("TR");
+  noticeRow.innerHTML =
+    " <td class='cart--info' colspan='4'>Add Products to Cart</td>";
+  noticeRow.classList.add(".no-items");
+  table.appendChild(noticeRow);
+}
+checkoutProduct();
